bbf667ab8cbd644b92f072dc72e5fd5285b83b37
christinekwon <christinekwon22@gmail.com>
2024-09-06 15:44:42 -0400

    replace sanity files

---
+++ src/sanity/desk/art.jsx
@@ -0,0 +1,37 @@
+import { HeartFilledIcon, DocumentIcon } from '@sanity/icons';
+import { orderableDocumentListDeskItem } from '@sanity/orderable-document-list';
+import { apiVersion } from '@/sanity/env';
+
+const artIndexPage = (S) => {
+	return S.listItem()
+		.title('Index Page')
+		.child(
+			S.editor()
+				.id('pArtIndex')
+				.title('Art Index Page')
+				.schemaType('pArtIndex')
+				.documentId('pArtIndex')
+		)
+		.icon(DocumentIcon);
+};
+
+export const artMenu = (S, context) => {
+	return S.listItem()
+		.icon(HeartFilledIcon)
+		.title('Art')
+		.id('art')
+		.child(
+			S.list()
+				.title('Art')
+				.items([
+					artIndexPage(S),
+					orderableDocumentListDeskItem({
+						title: 'Art Projects',
+						type: 'pArt',
+						icon: HeartFilledIcon,
+						S,
+						context,
+					}),
+				])
+		);
+};
--- src/sanity/deskStructure.js
+++ src/sanity/deskStructure.js
@@ -1,28 +1,53 @@
-import { globalMenu } from './desk/global'
-import { pagesMenu, otherPagesMenu } from './desk/pages'
-import { pageBlog } from './desk/p-blog'
-import { menusMenu } from './desk/menus'
-import { colorsMenu } from './desk/colors'
-import { settingsMenu } from './desk/settings'
-// import { artMenu } from './desk/art'
-// import { designMenu } from './desk/design'
+// import { globalMenu } from './desk/global';
+// import { pagesMenu, otherPagesMenu } from './desk/pages';
+// import { pageBlog } from './desk/p-blog';
+// import { menusMenu } from './desk/menus';
+// import { colorsMenu } from './desk/colors';
+// import { settingsMenu } from './desk/settings';
+
+// const deskStructure = (S) =>
+// 	S.list()
+// 		.title('Website')
+// 		.items([
+// 			globalMenu(S),
+// 			pagesMenu(S),
+// 			otherPagesMenu(S),
+// 			S.divider(),
+// 			pageBlog(S),
+// 			S.divider(),
+// 			menusMenu(S),
+// 			colorsMenu(S),
+// 			S.divider(),
+// 			settingsMenu(S),
+// 		]);
+
+// export default deskStructure;
+
+import { globalMenu } from './desk/global';
+import { pagesMenu } from './desk/pages';
+// import { pageBlog } from './desk/p-blog';
+// import { menusMenu } from './desk/menus';
+// import { colorsMenu } from './desk/colors';
+import { settingsMenu } from './desk/settings';
+// import { artMenu } from './desk/art';
+import { designMenu } from './desk/design';
 
 const deskStructure = (S, context) =>
-  S.list()
-    .title('Website')
-    .items([
-      globalMenu(S),
-      pagesMenu(S),
-      // artMenu(S, context),
-      // designMenu(S),
-      // otherPagesMenu(S),
-      // S.divider(),
-      // pageBlog(S),
-      // S.divider(),
-      // menusMenu(S),
-      // colorsMenu(S),
-      S.divider(),
-      settingsMenu(S),
-    ])
+	S.list()
+		.title('Website')
+		.items([
+			globalMenu(S),
+			pagesMenu(S),
+			// artMenu(S, context),
+			designMenu(S),
+			// otherPagesMenu(S),
+			// S.divider(),
+			// pageBlog(S),
+			// S.divider(),
+			// menusMenu(S),
+			// colorsMenu(S),
+			S.divider(),
+			settingsMenu(S),
+		]);
 
-export default deskStructure
+export default deskStructure;
