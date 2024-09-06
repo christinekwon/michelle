7ffa233a023c52a12c3cdbe6f8999fa0cc9bd6bf
christinekwon <christinekwon22@gmail.com>
2024-09-06 17:38:00 -0400

    responsive header variables

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -6,6 +6,7 @@ import Link from 'next/link'
 import Menu from '@/components/Menu'
 import MobileMenuTrigger from './mobile-menu-trigger'
 import Img from '@/components/Image'
+import useWindowDimensions from '@/hooks/useWindowDimensions'
 
 export default function Header({ siteData, data }) {
   const pathname = usePathname()
@@ -13,14 +14,14 @@ export default function Header({ siteData, data }) {
   const headerTitleRef = useRef()
   const headerMenuRef = useRef()
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
-
+  const { width, height } = useWindowDimensions()
   const { keyText, menu, mobileImage } = data
 
   useEffect(() => {
     document.documentElement.style.setProperty('--s-header-y', `${headerTitleRef?.current?.offsetHeight || 0}px`)
     const links = document.querySelector('.js-header-links')
     links && document.documentElement.style.setProperty('--header-menu-width', `${links.clientWidth}px`)
-  }, [])
+  }, [width])
 
   const onToggleMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen)
